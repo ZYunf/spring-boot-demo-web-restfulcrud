cdfe032ca139f0035f6f298e24a305c35bbccc0a
zyf_glb <845951122@qq.com>
2019-03-01 18:33:57 +0800

    Initial commit

---
+++ src/main/java/com/zyunf/springboot/SpringBootDemoWebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.zyunf.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoWebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoWebRestfulcrudApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zyunf/springboot/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.zyunf.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: Yunf
+ * @Date: 2019/3/1 17:28
+ * @ProjectName: spring-boot-demo-web-restfulcrud
+ * @PackageName: com.zyunf.springboot.controller
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+
+}
